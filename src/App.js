40b81ac3e3a8e085ec32708ed2854cb690518bdf
ahmed mansour <85390397+ahmedmansour2002@users.noreply.github.com>
2023-04-06 16:46:34 +0200

    Previous Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ComponentDidMount from './components/ComponentDidMount';
-import FirstFetch from './fetch Api/FirstFetch';
+import ComponentDidMount from './main/components/ComponentDidMount';
+import FirstFetch from './main/fetch Api/FirstFetch';
+import PreviousPropsState from './main/previous props';
 
 function App() {
   return (
     <div className='container' style={{ textAlign: "center", marginTop: "1rem" }}>
       <ComponentDidMount /><hr />
-      <FirstFetch />
+      <FirstFetch /><hr />
+      <PreviousPropsState /><hr />
     </div>
   );
 }
---
+++ src/main/previous props/PreviousProps.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function PreviousPropsState() {
+    const [term, setTerm] = useState('javascript')
+    const previousTermState = useRef();
+
+    useEffect(() => {
+        previousTermState.current = term;
+    }, [term])
+
+    const prevTerm = previousTermState.current;
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <div className='my-3'>
+                        <label htmlFor='exampleFormControlInput1' className='form-label'>
+                            Search Input
+                        </label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='exampleFormControlInput1'
+                            onChange={(e) => setTerm(e.target.value)}
+                            value={term}
+                        />
+                    </div>
+                    <p>Current term: "{term}"</p>
+                    <p>prev term: "{prevTerm}"</p>
+                </div>
+            </div>
+        </div>
+    )
+}
